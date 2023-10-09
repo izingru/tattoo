cd587161a7cc456d03ba3af7d2f16ef698455878
izingru <146376594+izingru@users.noreply.github.com>
2023-10-10 01:15:01 +0300

    Add files via upload
    plugin before after image

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-(function ($) {
-  $(function () {
+var divisor1 = document.getElementById('js-divisor-1'),
+  slider1 = document.getElementById('js-slider-1');
 
+function moveDivisor() {
+  divisor1.style.width = slider1.value+'%';
+}
 
-    var $dragMe = $(".dragme"),
-      $container = $(".sl-container"),
-      $viewAfter = $(".view-after");
-    $dragMe.draggable({
-      containment: "parent",
-      drag: function() {
-        $viewAfter.css({
-          width : parseFloat($(this).css('left')) + 5
-        });
-      }
-    });
-    $container.on("click", function(event) {
-      var eventLeft = event.pageX - $container.offset().left - 15;
-      animateTo(eventLeft);
-    });
-    animateTo("50%");
-    function animateTo(_left) {
-      $dragMe.animate({
-        left: _left
-      }, 'slow', 'linear');
-      $viewAfter.animate({
-        width: _left
-      }, 'slow', 'linear');
-    }
+var divisor2 = document.getElementById('js-divisor-2'),
+  slider2 = document.getElementById('js-slider-2');
 
+function moveDivisor2() {
+  divisor2.style.width = slider2.value+'%';
+}
 
+var divisor3 = document.getElementById('js-divisor-3'),
+  slider3 = document.getElementById('js-slider-3');
 
-  });
-})(jQuery);
+function moveDivisor3() {
+  divisor3.style.width = slider3.value+'%';
+}
+
+var divisor4 = document.getElementById('js-divisor-4'),
+  slider4 = document.getElementById('js-slider-4');
+
+function moveDivisor4() {
+  divisor4.style.width = slider4.value+'%';
+}
+
+var divisor5 = document.getElementById('js-divisor-5'),
+  slider5 = document.getElementById('js-slider-5');
+
+function moveDivisor5() {
+  divisor5.style.width = slider5.value+'%';
+}
